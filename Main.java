026f94c7153cee676f06709ada414b728fd6d809
MatheusEch <62758834+MatheusEch@users.noreply.github.com>
2020-07-23 11:10:04 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,59 +7,64 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("What you want to do?/n");
-        System.out.println("1- Register a new person account.");
-        System.out.println("2- Register a new enterprise account.");
-        System.out.println("3- List all accounts.");
-        System.out.println("4- Exit.");
-
         Scanner input = new Scanner(System.in);
-        boolean keep = true;
         Account account = new Account();
         PersonAccount pAccount = new PersonAccount();
         EnterpriseAccount eAccount = new EnterpriseAccount();
-        String option = input.nextLine();
+        int option;
 
         do{
-            if(option.equals("1")) {
-                System.out.println("Person name: ");
-                pAccount.setName(input.nextLine());
-                System.out.println("Person SNN: ");
-                pAccount.setSnn(input.nextLine());
-                System.out.println("Account number: ");
-                pAccount.setAccNumber(input.nextLine());
-                System.out.println("Amount in account: ");
-                pAccount.setAmount(input.nextDouble());
-                System.out.println("Account limit: ");
-                pAccount.setLimit(input.nextDouble());
-                break;
-            }
-            if(option.equals("2")){
-                System.out.println("Enterprise name: ");
-                eAccount.setName(input.nextLine());
-                System.out.println("Enterprise CNPJ: ");
-                eAccount.setCnpj(input.nextLine());
-                System.out.println("Account number: ");
-                eAccount.setAccNumber(input.nextLine());
-                System.out.println("Amount in account: ");
-                eAccount.setAmount(input.nextDouble());
-                System.out.println("Account limit; ");
-                eAccount.setLimit(input.nextDouble());
-                break;
-            }
-            if(option.equals("3")){
-                //print list
-            }
-            if(option.equals("4")){
-                keep = false;
-                break;
-            }
-        } while(keep);
+            System.out.println("What you want to do?");
+            System.out.println("1- Register a new person account.");
+            System.out.println("2- Register a new enterprise account.");
+            System.out.println("3- List all accounts.");
+            System.out.println("4- Exit.");
 
-//        Account acc;
- //       for (int i=0; i<listAccount.size();i++){
- //           acc = listAccount.get(i);
-//            System.out.println(acc.getLimit() + " to ");
-//            System.out.println(acc.getLimit());
+            option = input.nextInt();
+
+            switch(option) {
+
+                case 1:
+                    System.out.print("Person name: ");
+                    pAccount.setName(input.nextLine());
+                    System.out.print("Person SNN: ");
+                    pAccount.setSnn(input.nextLine());
+                    System.out.print("Account number: ");
+                    pAccount.setAccNumber(input.nextLine());
+                    System.out.print("Amount in account: ");
+                    pAccount.setAmount(input.nextDouble());
+                    System.out.println("Account limit: ");
+                    pAccount.setLimit(input.nextDouble());
+                    break;
+
+                case 2:
+                    System.out.print("Enterprise name: ");
+                    eAccount.setName(input.nextLine());
+                    System.out.print("Enterprise CNPJ: ");
+                    eAccount.setCnpj(input.nextLine());
+                    System.out.print("Account number: ");
+                    eAccount.setAccNumber(input.nextLine());
+                    System.out.print("Amount in account: ");
+                    eAccount.setAmount(input.nextDouble());
+                    System.out.println("Account limit; ");
+                    eAccount.setLimit(input.nextDouble());
+                    break;
+
+                case 3:
+                    //print list
+                    break;
+
+                case 4:
+                    break;
+                }
+            } while(option != 4);
+
+        ArrayList<PersonAccount> pAccounts = new ArrayList<PersonAccount>();
+//      Account acc;
+//      for (int i=0; i<listAccount.size();i++){
+//          acc = listAccount.get(i);
+//          System.out.println(acc.getLimit() + " to ");
+//          System.out.println(acc.getLimit());
+//        }
     }
 }
